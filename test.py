aedd1061c450113e06f07684b1b8add328d8c979
antrn <2446026601@qq.com>
2021-12-19 17:49:05 +0800

    add tieba

--- test.py
+++ test.py
@@ -24,19 +24,25 @@ def v2ex():
         driver.quit()
 
 @retry(stop_max_attempt_number=5)
-def niuke():
+def tieba():
     try:
         driver = webdriver.Chrome()
-        driver.get("https://www.nowcoder.com/ta/huawei")
-        driver.find_element_by_xpath("//*[@placeholder='用户名或电子邮箱地址']").send_keys(username)
-        driver.find_element_by_xpath("//*[@type='password']").send_keys(password) 
-        valid = Ocr_Captcha(driver, "//input[starts-with(@style,'background-image')]", img_path) # 验证码识别
-        driver.find_element_by_xpath("//*[@placeholder='请输入上图中的验证码']").send_keys(password)
-        driver.find_element_by_xpath("//*[@type='submit']").click()
+        driver.get("https://tieba.baidu.com/index.html")
+        driver.find_element_by_xpath("//*[@id='com_userbar']/ul/li[4]/div/a").click()
+        time.sleep(1)
+        driver.find_element_by_xpath("//*[@id='TANGRAM__PSP_4__footerULoginBtn']").click()
+        time.sleep(1)
+        driver.find_element_by_xpath("//*[@id='TANGRAM__PSP_4__userName']").send_keys(username)
+        driver.find_element_by_xpath("//*[@id='TANGRAM__PSP_4__password']").send_keys(password)
+        driver.find_element_by_xpath("//*[@id='TANGRAM__PSP_4__submit']").click()
 
         time.sleep(2)
-        driver.get('https://v2ex.com/mission/daily')
-        driver.find_element_by_xpath("//*[@type='button']").click()
+        driver.get('https://tieba.baidu.com/index.html')
+        driver.find_element_by_xpath("//*[@id='onekey_sign']/a").click()
+        time.sleep(1)
+        if driver.find_element_by_xpath("//*[@id='dialogJbody']/div/div/div[1]/a") != []:
+            driver.find_element_by_xpath("//*[@id='dialogJbody']/div/div/div[1]/a").click()
+            print("tieba签到成功")
     except:
         raise
     finally:
@@ -44,4 +50,4 @@ def niuke():
 
 if __name__ == '__main__':
     # v2ex()
-    niuke()
+    tieba()
