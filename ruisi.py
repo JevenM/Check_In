c0d8019d8f2669c5fd656ec703cac71f7ba772ec
antrn <2446026601@qq.com>
2021-12-20 14:16:30 +0800

    更新睿思

--- ruisi.py
+++ ruisi.py
@@ -22,15 +22,18 @@ def ruisi():
         time.sleep(4)
         driver.get("http://rs.xidian.edu.cn/plugin.php?id=dsu_paulsign:sign")
 
-        if driver.find_element_by_xpath("//*[@id='kx']") !=[]:
-            driver.find_element_by_xpath("//*[@id='kx']").click()
-            driver.find_element_by_xpath("//*[@id='todaysay']").send_keys("Hello World!")
-
-        if driver.find_element_by_xpath("//*[@id='qiandao']/table[1]/tbody/tr/td/div/a") !=[]:
-            driver.find_element_by_xpath("//*[@id='qiandao']/table[1]/tbody/tr/td/div/a").click()
-            print('ruisi签到成功')
+        if driver.find_element_by_xpath("/html/body/div[7]/div[2]/div[1]/h1[1]").text == "您今天已经签到过了或者签到时间还未开始":
+            print("您今天已经签到过了或者签到时间还未开始")
         else:
-            print("ruisi签到失败")
+            if driver.find_element_by_xpath("//*[@id='kx']") !=[]:
+                driver.find_element_by_xpath("//*[@id='kx']").click()
+                driver.find_element_by_xpath("//*[@id='todaysay']").send_keys("Hello World!")
+
+            if driver.find_element_by_xpath("//*[@id='qiandao']/table[1]/tbody/tr/td/div/a") !=[]:
+                driver.find_element_by_xpath("//*[@id='qiandao']/table[1]/tbody/tr/td/div/a").click()
+                print('ruisi签到成功')
+            else:
+                print("ruisi签到失败")
     except:
         raise
     finally:
